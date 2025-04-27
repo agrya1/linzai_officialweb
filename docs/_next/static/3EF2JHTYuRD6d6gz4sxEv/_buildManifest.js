84f0a80b36d6c5c8e1cc6d22a541e3a491c8af41
weimeiddy <weimeiddy@gmail.com>
2025-04-27 22:54:28 +0800

    添加静态网站文件到docs目录用于GitHub Pages部署

---
+++ docs/_next/static/3EF2JHTYuRD6d6gz4sxEv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/216-efd7d5e2528dfdc3.js","static/chunks/pages/index-f7907bfca9aa275c.js"],"/_error":["static/chunks/pages/_error-08a9db0f433628d8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
